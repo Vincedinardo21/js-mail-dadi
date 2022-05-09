fb1865d1577d438805f0334f12d8bc74779ff5ec
vince <vincedinardo21@gmail.com>
2022-05-09 18:16:09 +0200

    Email da completare

--- Dadi/myscript.js
+++ Dadi/myscript.js
@@ -3,6 +3,8 @@ var numeriDado = [1, 2, 3, 4, 5, 6];
 const clickButton = document.getElementById("myButton");
 const myOutput = document.querySelector(".number");
 const pcOutput = document.querySelector(".numberPC");
+const esitoFinale = document.querySelector(".esito");
+
 
 // Scelgo un numero casuale tra gli elementi dell'array
 const random = Math.floor(Math.random() * numeriDado.length);
@@ -27,8 +29,12 @@ clickButton.addEventListener("click",
 
 if(numeriDado[random] > numeriDado[randomPC]) {
     console.log("Hai vinto");
+    esitoFinale.innerHTML = "Hai vinto!";
+    
 } else if(numeriDado[random] == numeriDado[randomPC]) {
     console.log("Pareggio");
+    esitoFinale.innerHTML = "Pareggio!";
 } else {
     console.log("Hai perso");
+    esitoFinale.innerHTML = "Hai perso!";
 }
---
+++ Email/myscript.js
@@ -0,0 +1,8 @@
+var listaEmail = ["marco@gmail.com", "luca@gmail.com", "pippo@gmail.com"];
+const myEmail = document.querySelector("input");
+
+if(myEmail == listaEmail){
+    console.log("Puoi entrare");
+} else {
+    console.log("Non puoi entrare");
+}
