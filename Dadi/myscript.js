183e7a94864d52e56bd94e90096c2176b165086f
vince <vincedinardo21@gmail.com>
2022-05-09 16:24:05 +0200

    Numero pc

--- Dadi/myscript.js
+++ Dadi/myscript.js
@@ -2,15 +2,25 @@
 var numeriDado = ["1", "2", "3", "4", "5", "6"];
 const clickButton = document.getElementById("myButton");
 const myOutput = document.querySelector(".number");
+const pcOutput = document.querySelector(".numberPC");
 
 // Scelgo un numero casuale tra gli elementi dell'array
 const random = Math.floor(Math.random() * numeriDado.length);
+
+// Il PC sceglie un numero casuale tra gli elementi dell'array
+const randomPC = Math.floor(Math.random() * numeriDado.length);
 console.log(numeriDado[random]);
 
-// Stampo in pagina
+// Stampo in pagina il mio numero
 clickButton.addEventListener("click",
     function(){
-        console.log("clicked");
         myOutput.innerHTML = numeriDado[random];
+    }
+);
+
+// Stampo in pagina il numero del PC
+clickButton.addEventListener("click",
+    function(){
+        pcOutput.innerHTML = numeriDado[randomPC];
     }
 );
