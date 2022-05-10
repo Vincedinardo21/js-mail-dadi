2d7d5a80d84b068e9cb30f998ea5247bf450b40f
vince <vincedinardo21@gmail.com>
2022-05-10 14:32:23 +0200

    Fix dadi

--- Dadi/myscript.js
+++ Dadi/myscript.js
@@ -1,5 +1,5 @@
 // Variabili
-var numeriDado = [1, 2, 3, 4, 5, 6];
+// var numeriDado = [1, 2, 3, 4, 5, 6];
 const clickButton = document.getElementById("myButton");
 const myOutput = document.querySelector(".number");
 const pcOutput = document.querySelector(".numberPC");
@@ -7,26 +7,26 @@ const esitoFinale = document.querySelector(".esito");
 
 
 // Scelgo un numero casuale tra gli elementi dell'array
-const random = Math.floor(Math.random() * numeriDado.length);
+const random = Math.floor(Math.random() * 6) + 1;
 
 // Il PC sceglie un numero casuale tra gli elementi dell'array
-const randomPC = Math.floor(Math.random() * numeriDado.length);
-console.log(numeriDado[random]);
+const randomPC = Math.floor(Math.random() * 6) + 1;
+// console.log(numeriDado[random]);
 
 // Al click escono i risultati
 clickButton.addEventListener("click",
     function(){
         // Stampo del mio numero
-        myOutput.innerHTML = "Numero dado giocatore: " + numeriDado[random];
+        myOutput.innerHTML = "Numero dado giocatore: " + random;
 
         // Stampo numero del PC
-        pcOutput.innerHTML = "Numero dado PC: " + numeriDado[randomPC];
+        pcOutput.innerHTML = "Numero dado PC: " + randomPC;
         
         // Stampo dell'esito
-        if(numeriDado[random] > numeriDado[randomPC]) {
+        if(random > randomPC) {
             console.log("Hai vinto");
             esitoFinale.innerHTML = "Hai vinto!";          
-        } else if(numeriDado[random] == numeriDado[randomPC]) {
+        } else if(random == randomPC) {
             console.log("Pareggio");
             esitoFinale.innerHTML = "Pareggio!";
         } else {
